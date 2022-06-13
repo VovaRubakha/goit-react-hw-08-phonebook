6a24cbf38dcee204b2016d09fc123faf66645cf2
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-06-13 10:33:34 +0300

    add HomePage styles and Title

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,9 @@ const HomePage = ()=> {
     return (
         <Section  title="HomePage" >
             <div className={styles.container}>
-                Hello
+                <img src="https://www.iconpacks.net/icons/1/free-phone-book-icon-1112-thumb.png" alt="Phonebook" />
+
+                <h1 className={styles.title}>Can't remember any phone number, This phonebook can help you. Enjoy! </h1>
             </div>
         </Section>
     )
